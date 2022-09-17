5fb70d106f7a797d0439ca73c2834a28c19c7ac2
eduviezzer <33733608+viezzer@users.noreply.github.com>
2022-09-17 20:56:55 -0300

    Criado classe BoletoBancario.java
    ainda em andamento

---
+++ src/model/BoletoBancario.java
@@ -0,0 +1,23 @@
+package model;
+
+public class BoletoBancario extends Documento {
+	//Atributos
+	private int codBanco;
+	private int nossoNumero;
+	private int carteira;
+	private int usoDoBanco;
+	private double valor;
+	private double desconto;
+	private double juros;
+	private double valorCobrado;
+	private boolean aceite;
+	private String linhaDigitavel;
+	private String cedente;
+	private String pagador;
+	private String info;
+	private String especie;
+	private String moeda;
+	private Data vencimento;
+	private Data emissao;
+
+}
